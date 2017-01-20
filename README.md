704c91cdbf03fa82599f9ad4e6d63b28a348da0f
sofferjacob <sofferjacob@gmail.com>
2017-01-20 17:21:16 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Sierra Toolchain
+This is the toolchain used to cross-compile Sierra from source. It is compatible with any kind of
+architecture (host).
+
+# Using it.
+You can build it and use the toolchain in any UNIX like OS (Linux, macOS, BSD). First clone the repository to your computer and run:
+```
+cd toolchain
+./build.sh
+```
+
+Wait a while for your toolchain to download and build, then you will have your toolchain installed in /opt/cross.
+
+Copyright (c) 2017 Jacobo Soffer.
---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Sierra Toolchain for UNIX like host systems.
+# Build script.
+# Copyright (c) 2017 Jacobo Soffer.
+echo Downloading sources…
+wget -i wget-list
+echo Extracting archives…
+for f in *.tar*; do tar xf $f; done
+echo Preparing to build…
+cd gcc-6.2.0
+ln -s ../mpfr-3.1.5 mpfr
+ln -s ../gmp-6.1.1 gmp
+ln -s ../mpc-1.0.3 mpc
+cd ..
+export PREFIX="$HOME/opt/cross"
+export TARGET=i686-elf
+export PATH="$PREFIX/bin:$PATH"
+echo $PREFIX
+echo $TARGET
+echo $PATH
+echo Building Binutils…
+mkdir binutils-build
+cd binutils-build
+../binutils-2.27/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+make -j2
+make install
+cd ..
+echo Building GCC…
+mkdir gcc-build
+cd gcc-build
+../gcc-6.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+make all-gcc
+make all-target-libgcc
+make install-gcc
+make install-target-libgcc
+make all-target-libssp
+make install-target-libssp
+echo Toolchain Built!
+echo Your toolchain was installed in $HOME/opt/cross
+ 
